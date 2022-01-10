3f7117ec238b40c675740a2b3c4f7cb821e06508
Alireza Sharifikia <alireza.sharifikia@gmail.com>
2022-01-10 22:08:10 +0900

    feat: Add Schema + Skillset + TL Content
    Resources:
    + Schema guidelines
    + Schema data of my personal info
    + Skillset fully updated
    + Timeline text content file added
    + Github.svg added

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,60 +1,129 @@
-import { SkillCat, Skill } from '../portfolio/features/skillTable/constants'
+// import { SkillCat, Skill } from '../portfolio/features/skillTable/constants'
 
-var SkillsData: Array<SkillCat> = [
+// var SkillsData: Array<SkillCat> = [
+//     {
+//         name: 'Frontend',
+//         data: []
+//     },
+//     {
+//         name: 'Backend',
+//         data: []
+//     },
+//     {
+//         name: 'Database',
+//         data: []
+//     }
+// ]
+// const newSkill = (
+//     n: string,
+//     v: number,
+//     libs: Array<Skill> | undefined = []
+// ): Skill => ({ name: n, expertise: v, libs: libs })
+// let JavascriptLib = new Array<Skill>()
+
+// JavascriptLib.push(newSkill('JQuery', 100))
+// JavascriptLib.push(newSkill('Vue.js', 90))
+// JavascriptLib.push(newSkill('AngularJS', 50))
+// JavascriptLib.push(newSkill('React', 70))
+
+// let PHPLib = new Array<Skill>()
+// PHPLib.push(newSkill('Laravel', 100))
+// PHPLib.push(newSkill('CakePHP', 80))
+
+// let CSSLib = new Array<Skill>()
+// CSSLib.push(newSkill('SASS', 100))
+// CSSLib.push(newSkill('SCSS', 100))
+// CSSLib.push(newSkill('Bootstrap', 100))
+
+// let PyLib = new Array<Skill>()
+// PyLib.push(newSkill('Django', 70))
+
+// let JavascriptLib2 = new Array<Skill>()
+// JavascriptLib2.push(newSkill('Node.js', 40))
+
+// SkillsData[0].data.push(newSkill('Typescript', 100))
+// SkillsData[0].data.push(newSkill('Javascript', 100, JavascriptLib))
+// SkillsData[0].data.push(newSkill('CSS', 100, CSSLib))
+// SkillsData[0].data.push(newSkill('HTML', 100))
+// SkillsData[1].data.push(newSkill('PHP', 100, PHPLib))
+// SkillsData[1].data.push(newSkill('Python', 80, PyLib))
+// SkillsData[1].data.push(newSkill('Go', 70))
+// SkillsData[1].data.push(newSkill('Typescript', 100, JavascriptLib2))
+// SkillsData[1].data.push(newSkill('Javascript', 100, JavascriptLib2))
+// SkillsData[1].data.push(newSkill('C++', 80))
+// SkillsData[1].data.push(newSkill('Java', 55))
+// SkillsData[2].data.push(newSkill('MySQL', 100))
+// SkillsData[2].data.push(newSkill('PostgreS', 60))
+// SkillsData[2].data.push(newSkill('SQLite', 90))
+// SkillsData[2].data.push(newSkill('MongoDB', 90))
+
+// console.log(JSON.stringify(SkillsData))
+// export default SkillsData
+export default [
     {
         name: 'Frontend',
-        data: []
+        data: [
+            { name: 'Typescript', expertise: 100, libs: [] },
+            {
+                name: 'Javascript',
+                expertise: 100,
+                libs: [
+                    { name: 'JQuery', expertise: 100, libs: [] },
+                    { name: 'Vue.js', expertise: 90, libs: [] },
+                    { name: 'AngularJS', expertise: 50, libs: [] },
+                    { name: 'React', expertise: 70, libs: [] }
+                ]
+            },
+            {
+                name: 'CSS',
+                expertise: 100,
+                libs: [
+                    { name: 'SASS', expertise: 100, libs: [] },
+                    { name: 'SCSS', expertise: 100, libs: [] },
+                    { name: 'Bootstrap', expertise: 100, libs: [] }
+                ]
+            },
+            { name: 'HTML', expertise: 100, libs: [] }
+        ]
     },
     {
         name: 'Backend',
-        data: []
+        data: [
+            {
+                name: 'PHP',
+                expertise: 100,
+                libs: [
+                    { name: 'Laravel', expertise: 100, libs: [] },
+                    { name: 'CakePHP', expertise: 80, libs: [] }
+                ]
+            },
+            {
+                name: 'Python',
+                expertise: 80,
+                libs: [{ name: 'Django', expertise: 70, libs: [] }]
+            },
+            { name: 'Go', expertise: 70, libs: [] },
+            {
+                name: 'Typescript',
+                expertise: 100,
+                libs: [{ name: 'Node.js', expertise: 40, libs: [] }]
+            },
+            {
+                name: 'Javascript',
+                expertise: 100,
+                libs: [{ name: 'Node.js', expertise: 40, libs: [] }]
+            },
+            { name: 'C++', expertise: 80, libs: [] },
+            { name: 'Java', expertise: 55, libs: [] }
+        ]
     },
     {
         name: 'Database',
-        data: []
+        data: [
+            { name: 'MySQL', expertise: 100, libs: [] },
+            { name: 'PostgreS', expertise: 60, libs: [] },
+            { name: 'SQLite', expertise: 90, libs: [] },
+            { name: 'MongoDB', expertise: 90, libs: [] }
+        ]
     }
 ]
-const newSkill = (
-    n: string,
-    v: number,
-    libs: Array<Skill> | undefined = []
-): Skill => ({ name: n, expertise: v, libs: libs })
-let JavascriptLib = new Array<Skill>()
-
-JavascriptLib.push(newSkill('JQuery', 100))
-JavascriptLib.push(newSkill('Vue.js', 90))
-JavascriptLib.push(newSkill('AngularJS', 50))
-JavascriptLib.push(newSkill('React', 70))
-
-let PHPLib = new Array<Skill>()
-PHPLib.push(newSkill('Laravel', 100))
-PHPLib.push(newSkill('CakePHP', 80))
-
-let CSSLib = new Array<Skill>()
-CSSLib.push(newSkill('SASS', 100))
-CSSLib.push(newSkill('SCSS', 100))
-CSSLib.push(newSkill('Bootstrap', 100))
-
-let PyLib = new Array<Skill>()
-PyLib.push(newSkill('Django', 70))
-
-let JavascriptLib2 = new Array<Skill>()
-JavascriptLib2.push(newSkill('Node.js', 40))
-
-SkillsData[0].data.push(newSkill('Typescript', 100))
-SkillsData[0].data.push(newSkill('Javascript', 100, JavascriptLib))
-SkillsData[0].data.push(newSkill('CSS', 100, CSSLib))
-SkillsData[0].data.push(newSkill('HTML', 100))
-SkillsData[1].data.push(newSkill('PHP', 100, PHPLib))
-SkillsData[1].data.push(newSkill('Python', 80, PyLib))
-SkillsData[1].data.push(newSkill('Go', 70))
-SkillsData[1].data.push(newSkill('Typescript', 100, JavascriptLib2))
-SkillsData[1].data.push(newSkill('Javascript', 100, JavascriptLib2))
-SkillsData[1].data.push(newSkill('C++', 80))
-SkillsData[1].data.push(newSkill('Java', 55))
-SkillsData[2].data.push(newSkill('MySQL', 100))
-SkillsData[2].data.push(newSkill('PostgreS', 60))
-SkillsData[2].data.push(newSkill('SQLite', 90))
-SkillsData[2].data.push(newSkill('MongoDB', 90))
-
-export default SkillsData
---
+++ src/data/timelineContent.tsx
@@ -0,0 +1,31 @@
+export default {
+    uni: {
+        period: {
+            start: new Date(2015, 9),
+            end: new Date(2019, 9),
+        },
+        title: [<div>Beginning of Bachelor's Study</div>],
+        intro: [
+            <div>
+                I began my bachelor's in Electrical Engineering field and
+                pursued semiconductor devices, green energy harvesting
+                technologies, piezoelectrics and photonics.
+            </div>,
+        ],
+        text: [],
+    },
+    cc: {
+        period: {
+            start: new Date(2016, 6),
+            end: new Date(2017, 4, 20),
+        },
+        title: [<div>Internship As a Junior Web Developer</div>],
+        intro: [
+            <div>
+                Development of a web app that functioned as a portfolio and as a
+                supply chain for its different branches through out the country.
+            </div>,
+        ],
+        text: [],
+    },
+};
