9c20fdb2aeb05e83468cb2373103f21670eb3b84
Alireza Sharifikia <alireza.sharifikia@gmail.com>
2022-01-16 18:13:59 +0900

    feat: Add Loader Module

---
+++ src/portfolio/features/timeline/components/Loader/Loader.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./Loader.module.sass";
+
+interface Props {
+    show: boolean;
+}
+
+export default class Loader extends React.Component<Props> {
+    constructor(props: React.ComponentProps<any>) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div
+                className={styles.wrapper}
+                data-visibility={this.props.show ? "on" : "off"}
+            >
+                {/* <span></span> */}
+                <div className={styles.within}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-broadcast"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
+                    </svg>
+                </div>
+            </div>
+        );
+    }
+}
