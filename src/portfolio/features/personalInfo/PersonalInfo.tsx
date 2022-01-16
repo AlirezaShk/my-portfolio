a46f760eb03bed50e0bb3fb0483096da3cdb992e
Alireza Sharifikia <alireza.sharifikia@gmail.com>
2022-01-16 18:13:35 +0900

    feat: Add PInfo Mod., BG Animation

---
+++ src/portfolio/features/personalInfo/PersonalInfo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./PersonalInfo.module.sass";
+
+interface Props {
+    id: string;
+    title: string;
+}
+export default class PersonalInfo extends React.Component<Props, any> {}
